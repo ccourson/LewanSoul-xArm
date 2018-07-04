391da59ee97c34e26e3daea32abc447cbc3b667d
ccourson <chris@chrisbot.com>
2018-07-04 15:22:48 -0400

    Begin UI communications with robot.

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # LewanSoul-xArm
+
+## Objective
+
+## USB Communications
+
+## xArm Controller USB Protocol
+
+test
+
+| First Header  | Second Header |
+| ------------- | ------------- |
+| Content Cell  | Content Cell  |
+| Content Cell  | Content Cell  |
+
+
 Uses https://github.com/mikeobrien/HidLibrary
+
+
+
+        ServoMove =             3,  // (byte)count (ushort)time { (byte)id (ushort)position }
+        GroupRunRepeat =        5,  // (byte)group[255=all] (byte)times 
+        GroupRun =              6,  // (byte)group (ushort)count[0=continuous]
+        GroupStop =             7,  // -none-
+        GroupErase =            8,  // (byte)group[255=all]
+        GroupSpeed =            11, // (byte)group (ushort)percentage
+        xServoOffsetWrite =      12, 
+        xServoOffsetRead =       13, 
+        xServoOffsetAdjust =     14,
+        GetBatteryVoltage =     15, // -none-; (ushort)millivolts
+        ServoOff =              20, // (byte)count { (byte)id }
+        ServoPositionRead =     21, // (byte)count { (byte)id }; (byte)count { (byte)id (byte)offset }
+        ServoPositionWrite =    22, // (byte)count { (byte)id }
+        ServoOffsetRead =       23, // (byte)count { (byte)id }; (byte)count { (byte)id (byte)offset }
+        ServoOffsetWrite =      24, // (byte)id (ushort)value
+        BusServoMoroCtrl =      26, // (byte)id (byte)??? (ushort)speed
+        BusServoInfoWrite =     27, // (byte)id (ushort)pos_min (ushort)pos_max (ushort)volt_min (ushort)volt_max
+                                    //         (ushort)temp_max (byte)led_status (byte)led_warning
+        BusServoInfoRead =      28  // -none-; (byte)id (ushort)pos_min (ushort)pos_max (ushort)volt_min (ushort)volt_max 
+                                    //         (ushort)temp_max (byte)led_status (byte)led_warning (byte)dev_offset
+                                    //         (ushort)pos (byte)temp (ushort)volt
--- VS/LewanSoul-xArm/xArmConsole/Robot.cs
+++ VS/LewanSoul-xArm/xArmConsole/Robot.cs
@@ -19,123 +19,146 @@ namespace xArmDotNet
 
         public bool IsConnected { get { return device != null && device.IsConnected; } }
 
-        public static HidDevice device;
+        public static HidDevice device = null;
+
+        public event EventHandler<OnReportReceivedEventArgs> OnReportReceived;
+        public event EventHandler OnConnected;
+        public event EventHandler OnDisconnected;
+
 
         public Robot()
         {
-            device = HidDevices.Enumerate(vendorId, productId).FirstOrDefault();
+            Connect();
+        }
 
-            if (device != null)
+        public void Connect()
+        {           
+            if (device == null)
             {
-                device.Inserted += DeviceAttachedHandler;
-                device.Removed += DeviceRemovedHandler;
+                device = HidDevices.Enumerate(vendorId, productId).FirstOrDefault();
 
-                device.MonitorDeviceEvents = true;
+                if (device != null)
+                {
+                    device.OpenDevice(DeviceMode.Overlapped, DeviceMode.Overlapped, ShareMode.ShareRead | ShareMode.ShareWrite);
+                    device.Inserted += DeviceAttachedHandler;
+                    device.Removed += DeviceRemovedHandler;
 
-                device.ReadReport(OnReport);
-                Debug.WriteLine("HidLibrary installed.");
+                    device.MonitorDeviceEvents = true;
+
+                    Debug.WriteLine("HidLibrary installed.");
+                }
             }
         }
 
-        private void OnReport(HidReport report)
+        protected void OnReport(HidReport report)
         {
-            if (!device.IsConnected) { return; }
-
-            Debug.WriteLine("HID Input Report: " + report.Data.ToString());
-            Debug.WriteLine("Total number of bytes received: " + report.Data.Length.ToString());
-
-            foreach (var item in report.Data)
+            if (!device.IsConnected)
             {
-                Debug.Write(string.Format("{0:X}-", item));
+                return;
             }
-            Debug.Write("");
+
+            //Debug.WriteLine(report.GetBytes().Length.ToString() + " bytes received: " + BitConverter.ToString(report.Data));
+
+            OnReportReceived?.Invoke(this, new OnReportReceivedEventArgs() { Data = report.Data });
 
             device.ReadReport(OnReport);
         }
 
         private void DeviceRemovedHandler()
         {
+            OnDisconnected?.Invoke(this, EventArgs.Empty);
             Debug.WriteLine("Robot disconnected.");
         }
 
         private void DeviceAttachedHandler()
         {
+            OnConnected?.Invoke(this, EventArgs.Empty);
+            device.ReadReport(OnReport);
             Debug.WriteLine("Robot connected.");
-            //foreach (var item in device.Capabilities.)
-            //{
-            //    Debug.WriteLine("   " + item.Key.ToString() + ": " + Convert.ToString(item.Value));
-            //}
-
-
         }
 
-        public void Connect()
+        private void SendHidReport(RobotCommand command, IBuffer parameters)
         {
-
-        }
-
-        public void Disconnect()
-        {
-
-        }
-
-        public int[] ReadServoOffsets(int[] servos)
-        {
-            return null;
-        }
-
-        public async void SendReport(RobotCommand command)
-        {
-            byte[] parameters = { };
-
-            HidReport report = device.CreateReport();
-
-            DataWriter dataWriter = new DataWriter() { ByteOrder = ByteOrder.LittleEndian };
-            dataWriter.WriteByte(0); // packet id
+            DataWriter dataWriter = new DataWriter();
+            dataWriter.WriteByte(0);        // packet id
             dataWriter.WriteUInt16(0x5555); // header
-            dataWriter.WriteByte((byte)(parameters.Length + 2));
-            dataWriter.WriteByte((byte)RobotCommand.BusServoInfoRead);
-            dataWriter.WriteBytes(parameters);
+            dataWriter.WriteByte((byte)(parameters.Length + 5));
+            dataWriter.WriteByte((byte)command);
+            dataWriter.WriteBuffer(parameters);
 
-            byte[] bytes = new byte[device.Capabilities.OutputReportByteLength - 1];
+            IBuffer buffer = dataWriter.DetachBuffer();
 
-            DataReader dataReader = new DataReader((IInputStream)dataWriter.DetachStream()) { ByteOrder = ByteOrder.LittleEndian };
-            dataReader.ReadBytes(bytes);
+            byte[] bytes = new byte[buffer.Length];
+            DataReader.FromBuffer(buffer).ReadBytes(bytes);
 
-            await device.WriteAsync(bytes);
-
-            Debug.WriteLine(report.Data.Length.ToString() + " bytes sent.");
+            device.Write(bytes);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="servos"></param>
+        public void GetServoOffsets(params int[] servos)
+        {
+            if (servos.Length == 0) throw new ArgumentNullException("At least one servo must be specified.");
+
+            DataWriter dataWriter = new DataWriter();
+            dataWriter.WriteByte((byte)servos.Length);         
+            dataWriter.WriteBytes(servos.Select(b => (byte)b).ToArray());
+
+            SendHidReport(RobotCommand.ServoOffsetRead, dataWriter.DetachBuffer());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="servos"></param>
+        public void GetServoAxes(params int[] servos)
+        {
+            if (servos.Length == 0) throw new ArgumentNullException("At least one servo must be specified.");
+
+            DataWriter dataWriter = new DataWriter();
+            dataWriter.WriteByte((byte)servos.Length);
+            dataWriter.WriteBytes(servos.Select(b => (byte)b).ToArray());
+
+            SendHidReport(RobotCommand.ServoPositionRead, dataWriter.DetachBuffer());
+        }
+
+        // TODO: Incomplete idea.
         public class RobotParameter
         {
             public byte motor;
             public uint position;
         }
+    }
 
-        public enum RobotCommand
-        {
-            ServoMove =             3,  // (byte)count (ushort)time { (byte)id (ushort)position }
-            GroupRunRepeat =        5,  // (byte)group[255=all] (byte)times 
-            GroupRun =              6,  // (byte)group (ushort)count[0=continuous]
-            GroupStop =             7,  // -none-
-            GroupErase =            8,  // (byte)group[255=all]
-            GroupSpeed =            11, // (byte)group (ushort)percentage
-            xServoOffsetWrite =      12, 
-            xServoOffsetRead =       13, 
-            xServoOffsetAdjust =     14,
-            GetBatteryVoltage =     15, // -none-; (ushort)millivolts
-            ServoOff =              20, // (byte)count { (byte)id }
-            ServoPositionRead =     21, // (byte)count { (byte)id }; (byte)count { (byte)id (byte)offset }
-            ServoPositionWrite =    22, // (byte)count { (byte)id }
-            ServoOffsetRead =       23, // (byte)count { (byte)id }; (byte)count { (byte)id (byte)offset }
-            ServoOffsetWrite =      24, // (byte)id (ushort)value
-            BusServoMoroCtrl =      26, // (byte)id (byte)??? (ushort)speed
-            BusServoInfoWrite =     27, // (byte)id (ushort)pos_min (ushort)pos_max (ushort)volt_min (ushort)volt_max
-                                        //         (ushort)temp_max (byte)led_status (byte)led_warning
-            BusServoInfoRead =      28  // -none-; (byte)id (ushort)pos_min (ushort)pos_max (ushort)volt_min (ushort)volt_max 
-                                        //         (ushort)temp_max (byte)led_status (byte)led_warning (byte)dev_offset
-                                        //         (ushort)pos (byte)temp (ushort)volt
-        }
+    public enum RobotCommand
+    {
+        ServoMove =             3,  // (byte)count (ushort)time { (byte)id (ushort)position }
+        GroupRunRepeat =        5,  // (byte)group[255=all] (byte)times 
+        GroupRun =              6,  // (byte)group (ushort)count[0=continuous]
+        GroupStop =             7,  // -none-
+        GroupErase =            8,  // (byte)group[255=all]
+        GroupSpeed =            11, // (byte)group (ushort)percentage
+        xServoOffsetWrite =      12, 
+        xServoOffsetRead =       13, 
+        xServoOffsetAdjust =     14,
+        GetBatteryVoltage =     15, // -none-; (ushort)millivolts
+        ServoOff =              20, // (byte)count { (byte)id }
+        ServoPositionRead =     21, // (byte)count { (byte)id }; (byte)count { (byte)id (byte)offset }
+        ServoPositionWrite =    22, // (byte)count { (byte)id }
+        ServoOffsetRead =       23, // (byte)count { (byte)id }; (byte)count { (byte)id (byte)offset }
+        ServoOffsetWrite =      24, // (byte)id (ushort)value
+        BusServoMoroCtrl =      26, // (byte)id (byte)??? (ushort)speed
+        BusServoInfoWrite =     27, // (byte)id (ushort)pos_min (ushort)pos_max (ushort)volt_min (ushort)volt_max
+                                    //         (ushort)temp_max (byte)led_status (byte)led_warning
+        BusServoInfoRead =      28  // -none-; (byte)id (ushort)pos_min (ushort)pos_max (ushort)volt_min (ushort)volt_max 
+                                    //         (ushort)temp_max (byte)led_status (byte)led_warning (byte)dev_offset
+                                    //         (ushort)pos (byte)temp (ushort)volt
+    }
+
+    public class OnReportReceivedEventArgs : EventArgs
+    {
+        public byte[] Data;
     }
 }
