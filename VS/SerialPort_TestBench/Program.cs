38fc25485f61fff22d418fd3ab239e0bb04ffd7f
ccourson <chris@chrisbot.com>
2019-02-01 17:40:38 -0500

    Create Program.cs

---
+++ VS/SerialPort_TestBench/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+
+namespace SerialPort_TestBench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort serialPort = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.Two);
+
+            serialPort.Open();
+            //* 0x03 (byte)count (ushort)milliseconds { (byte)servo (ushort)position[FF00=null] }
+            byte[] vs = new byte[] { 0x55, 0x55, 8, 3, 1, 0xe8, 3, 1, 0xbf, 1 };
+            serialPort.Write(vs, 0, vs.Length);
+            
+            while (true)
+            {
+                int data = serialPort.ReadByte();
+                if (data > 0)
+                {
+                    Debug.Write($"{data} ");
+                }
+            }
+        }
+    }
+}
